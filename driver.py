5c91dd5004292bbfa24e7c6891b76786d63d3b67
shunaky <shunaky@umich.edu>
2023-11-16 23:34:40 -0500

    Now keep track of  faces

--- driver.py
+++ driver.py
@@ -1,5 +1,8 @@
 import cv2
 import os
+from servo import ServoComplex
+
+os.system('sudo modprobe bcm2835-v4l2')
 
 FRAME_W = 320
 FRAME_H = 200
@@ -11,10 +14,16 @@ FRAME_W = 320
 FRAME_H = 200
 HORIZONTAL_MAX = 70 # degree
 VERTICAL_MAX = 30 # degree
+
+cascPath = 'lbpcascade_frontalface.xml'
+cascPath = 'haarcascade_frontalface_default.xml'
+faceCascade = cv2.CascadeClassifier(cascPath)
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,  320)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
+
+servos = ServoComplex()
 
 if not cap.isOpened():
 	print("error")
@@ -22,38 +31,35 @@ if not cap.isOpened():
 else:
 	print("opened")
 
-count = 0
-while count < 1000000:
-	count += 1
+while True:
 	ret, frame = cap.read()
-	#frame = cv2.flip(frame, -1)
 	
 	if ret == False:
-		#print("Error in Getting Image")
 		continue
-	else:
-		print("LETS GPPP")
+
 	# Convert to greyscale for easier faster accurate face detection
-	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #gray = cv2.equalizeHist(gray)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.equalizeHist(gray)
     
     # Do face detection to search for faces from these captures frames
-    #faces = faceCascade.detectMultiScale(frame, 1.1, 3, 0, (10, 10))
+	faces = faceCascade.detectMultiScale(frame, 1.1, 3, 0, (10, 10))
     
-    #for (x, y, w, h) in faces:
+	for (x, y, w, h) in faces:
         # Draw a green rectangle around the face (There is a lot of control to be had here, for example If you want a bigger border change 4 to 8)
-    #    cv2.rectangle(frame, (x, y), (x w, y h), (0, 255, 0), 4)
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 4)
         # Get the centre of the face
-     #   center_x = x + (w/2)
-      #  center_y = y + (h/2)
+		center_x = x + (w/2)
+		center_y = y + (h/2)
 
-        # get the percentage offset, relative to the center of image
-       # turn_x  = float(center_x - (FRAME_W/2)) / float(FRAME_W/2)
-        #turn_y  = float(center_y - (FRAME_H/2)) / float(FRAME_H/2)
+        # Get the percentage offset, relative to the center of image
+		turn_x  = float(center_x - (FRAME_W/2)) / float(FRAME_W/2)
+		turn_y  = float(center_y - (FRAME_H/2)) / float(FRAME_H/2)
         
-        # get the angles
-        #turn_horizontal = turn_x * HORIZONTAL_MAX
-	#	turn_vertical = turn_y * VERTICAL_MAX
+        # Get the angles
+		turn_horizontal = turn_x * HORIZONTAL_MAX
+		turn_vertical = turn_y * VERTICAL_MAX
+		
+		servos.move_servos(turn_horizontal, turn_vertical)
 		
 	cv2.imshow("Video", frame)
 	
--- servo.py
+++ servo.py
@@ -3,16 +3,16 @@ from gpiozero import AngularServo, Device
 
 PIN_1 = 17
 PIN_2 = 27
-HORIZONTAL_MIN = 0
-HORIZONTAL_MAX = 90
-VERTICAL_MIN = 0
-VERTICAL_MAX = 90
+HORIZONTAL_MIN = -70
+HORIZONTAL_MAX = 70
+VERTICAL_MIN = -30
+VERTICAL_MAX = 30
 
 class ServoComplex:
 	
 	def __init__(self):
-		self.servo_h = AngularServo(PIN_1, min_angle=HORIZONTAL_MIN, max_angle=HORIZONTAL_MAX)
-		self.servo_v = AngularServo(PIN_2, min_angle=VERTICAL_MIN, max_angle=VERTICAL_MAX)
+		self.servo_h = AngularServo(PIN_2, min_angle=HORIZONTAL_MIN, max_angle=HORIZONTAL_MAX)
+		self.servo_v = AngularServo(PIN_1, min_angle=VERTICAL_MIN, max_angle=VERTICAL_MAX)
 		
 	def move_horizontal(self, angle_h):
 		self.servo_h.angle = angle_h
@@ -21,10 +21,10 @@ class ServoComplex:
 		self.servo_v.angle = angle_v
 		
 	def move_servos(self, angle_h, angle_v):
-		self.move_horizontal(move_h)
-		self.move_vertical(move_h)
+		self.move_horizontal(angle_h)
+		self.move_vertical(angle_v)
 
-
+"""
 print("start")
 servo_1.angle = 0
 servo_2.angle = 90
@@ -42,3 +42,4 @@ servo_1.angle = 30
 servo_2.angle = 60
 time.sleep(2)
 print("end")
+"""
