a9ccfba7b76bdf99882b8760a680ed86fdf7e5b7
shunaky <shunaky@umich.edu>
2023-11-22 13:07:35 -0500

    Made a repo public and changed some

--- story_teller.py
+++ story_teller.py
@@ -1,6 +1,8 @@
 import requests
+import base64
 
 FILE_URL = "https://raw.githubusercontent.com/shun-aky/1001-StoryTeller/master/story.txt?token=GHSAT0AAAAAACC5PKVEFGLKD7HCVXAGGHIKZK6OB2Q"
+FILE_URL = 'https://api.github.com/repos/shun-aky/1001-StoryTeller/contents/story.txt'
 
 class StoryTeller:
     def __init__(self) -> None:
@@ -13,7 +15,10 @@ class StoryTeller:
             print(raw_url)
             response = requests.get(FILE_URL)
             if response.status_code == 200:
-                self.content = response.text
+                content = response.json()["content"]
+                content = content.encode("utf-8")
+                content = base64.b64decode(content).decode("utf-8")
+                self.content = content
                 print("Got story successfully!")
                 print(self.content)
             else:
